3e3e03c2fb4b2fb5e44384b6608a8d120085a069
birdofartemis <72250724+birdofartemis@users.noreply.github.com>
2021-04-25 16:21:38 +0000

    SQL Project - Projected Created

---
+++ src/com/company/db/DbException.java
@@ -0,0 +1,12 @@
+package com.company.db;
+
+import java.io.Serial;
+
+public class DbException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DbException(String msg){
+        super(msg);
+    }
+}
---
+++ src/com/company/db/DbIntegrityException.java
@@ -0,0 +1,13 @@
+package com.company.db;
+
+import java.io.Serial;
+
+public class DbIntegrityException extends RuntimeException{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DbIntegrityException(String msg){
+        super(msg);
+    }
+
+}
